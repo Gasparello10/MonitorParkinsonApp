0724004053cf146920909d7bc4cf5c5d808e446b
Gabriel Gasparello_ntieb <bielgasparello@gmail.com>
2025-09-03 10:26:15 -0300

    funcionando room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("kotlin-kapt")
 }
 
 android {
@@ -53,6 +54,10 @@ dependencies {
 
     implementation(project(":shared"))
     implementation (libs.androidx.localbroadcastmanager)
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
     // Vico Chart Library
     implementation("com.patrykandpatrick.vico:compose-m3:1.14.0")
     implementation("com.patrykandpatrick.vico:core:1.14.0")
